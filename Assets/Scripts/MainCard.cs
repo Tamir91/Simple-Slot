9f7d6586b045c876ee934b741c3623f6756c2ee6
Tamir <1hotlev@gmail.com>
2019-05-20 11:02:49 +0300

    first commit

---
+++ Assets/Scripts/MainCard.cs
@@ -0,0 +1,37 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCard : MonoBehaviour
+{
+    private int _id;
+     
+    [SerializeField] private GameObject Card_Back;
+    [SerializeField] private SceneControler controller;
+
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void ChangeSprite(int id, Sprite sprite)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+
+
+    private void OnMouseDown()
+    {
+        if (Card_Back.activeSelf && controller.canReveal )
+        {
+            Card_Back.SetActive(false);
+            controller.CardReveald(this);
+        }
+    }
+
+    public void Unreveal()
+    {
+        Card_Back.SetActive(true);
+    }
+}
